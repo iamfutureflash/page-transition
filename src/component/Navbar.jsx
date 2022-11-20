6983f16f64031daa4575e490307ae195821cbcbb
iamfutureflash <xenode2000@gmail.com>
2022-11-20 14:41:11 +0530

    simple page transition

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function Navbar() {
+  return (
+    <nav className="bg-slate-400" >
+      <ul className="" >
+        <li className="font-bold" >Home</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+export default Navbar;
